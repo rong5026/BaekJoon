1e2caa6d5613c068506c16d5310fe3b8c3315169
rong5026 <77156858+rong5026@users.noreply.github.com>
2023-05-30 16:53:56 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int x;
+        int y;
+        Scanner input = new Scanner(System.in);
+
+        x = input.nextInt();
+        y = input.nextInt();
+        System.out.println(x+y);
+    }
+}
