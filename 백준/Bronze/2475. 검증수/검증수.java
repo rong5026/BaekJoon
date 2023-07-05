404c2a0d7dbd1fdf692baa4d5d44c00494a3dc6b
rong5026 <77156858+rong5026@users.noreply.github.com>
2023-07-05 22:06:43 +0900

    [Bronze V] Title: 검증수, Time: 128 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] array = new int[5];
+
+        for (int i = 0 ; i < 5 ; i++)
+            array[i] = Integer.parseInt(st.nextToken());
+
+        int sum = 0;
+        for (int i = 0 ; i < 5 ; i++)
+            sum += array[i] * array[i];
+
+        System.out.println(sum % 10);
+
+    }
+}
