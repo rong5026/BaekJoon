d0575796fad4bd2e9f2ce230539ffe266494f191
rong5026 <77156858+rong5026@users.noreply.github.com>
2023-07-13 02:18:38 +0900

    [Gold III] Title: 나머지 합, Time: 520 ms, Memory: 120676 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -1,58 +1,48 @@
+import javax.swing.*;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.nio.Buffer;
+import java.sql.Array;
+import java.util.*;
+
+
 
 public class Main {
-    static int N, M;
-    static int A[];
-    static long D[]; // 구간합 배열
-    static long C[]; // 나머지가 같은 인덱스 개수 저장
-    
-	void Solve() {
-        long count = 0;
-        // 구간합 배열을 %M 한 나머지로 갱신
-        for (int i=0; i<N; i++) {
-            int remain = (int)(D[i] % M);
-            // 구간합 배열 요소에서 0인 인덱스 카운트
-            if (remain == 0) count++;
-            //D[i] = D[i]%M;
-            C[remain]++;
-        }
-        
-        // 구간합 배열의 부분 계산
-        for (int i=0; i<M; i++) {
-            if (C[i] <= 1) continue; // 2개 보다 이하면 패스
-            // 조합의 개수 구하는 공식 : C[i] 개수에서 2개를 고르는 모든 경우의 수
-            long tmp = C[i] * (C[i]-1)/ 2;
-            count += tmp;
-        }
-        System.out.println(count);
-	}
 
-    void inputData() throws Exception {
-        InputStreamReader reader = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(reader);
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        A = new int[N];
-        D = new long[N];
-        C = new long[M];
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        long cnt = 0;
+        long [] sum = new long[n];
+        long [] count = new long[m];
         st = new StringTokenizer(br.readLine());
-        for (int i=0; i<N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
-            if (i == 0) {
-                D[i] = A[i];
-            } else {
-                D[i] = D[i-1]+A[i];
+
+        sum[0] = Long.parseLong(st.nextToken());
+
+        for(int i = 1 ; i < n ; i++) {
+            sum[i] = sum[i-1] + Long.parseLong(st.nextToken());
+        }
+
+        for(int i = 0 ; i < n ; i++) {
+            sum[i] %= m;
+            if(sum[i] == 0)
+                cnt++;
+            count[(int) sum[i]]++;
+        }
+
+        for(int i = 0 ; i < m ; i++) {
+            if(count[i] >= 2) {
+                cnt += count[i] * (count[i] - 1) / 2;
+
             }
         }
-    }
 
-    public static void main(String[] args) throws Exception {
-        Main m = new Main();
-        m.inputData(); // 입력 받는 부분
-        m.Solve();// 여기서부터 작성
-    }
+        System.out.println(cnt);
 
+    }
 }
