338d0028fc3c1088c04bea196d358a6dde4699cd
rong5026 <77156858+rong5026@users.noreply.github.com>
2023-07-27 17:20:52 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1056 ms, Memory: 111780 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,29 +1,61 @@
-import javax.print.DocFlavor;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import javax.swing.*;
+import java.io.*;
+import java.nio.Buffer;
+import java.sql.Array;
 import java.util.*;
 
+
+
 public class Main {
+
+    public static int[] tmp;
+
+    private static void merge(int arr[], int start, int last, int mid) {
+        int i = start;
+        int j = mid + 1;
+        int k = start;
+
+        while (i <= mid && j <= last) {
+            if (arr[i] <= arr[j])
+                tmp[k++] = arr[i++];
+            else
+                tmp[k++] = arr[j++];
+        }
+        while (i <= mid)
+            tmp[k++] = arr[i++];
+        while (j <= last)
+            tmp[k++] = arr[j++];
+        for(int l = start ; l <=last ; l++)
+            arr[l] = tmp[l];
+    }
+    private static void merge_sort(int arr[], int start, int last) {
+
+        if (last - start < 1)
+            return ;
+        int mid = (start + last) / 2;
+        merge_sort(arr, start, mid);
+        merge_sort(arr, mid + 1, last);
+        merge(arr, start, last, mid);
+    }
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        ArrayList<Integer> list = new ArrayList<Integer>();
+        int n = Integer.parseInt(st.nextToken());
+        int []arr = new int[n];
+        tmp = new int[n];
 
-        for(int i = 0 ; i < n ; i++) {
-            int m = Integer.parseInt(br.readLine());
-            list.add(m);
+        for( int i = 0 ; i < n ; i++) {
+            arr[i]= Integer.parseInt(br.readLine());
         }
-        Collections.sort(list);
-
-        for(int i = 0 ; i < n ; i++) {
-           sb.append(list.get(i)).append("\n");
-        }
-        System.out.println(sb);
+        merge_sort(arr, 0 , n - 1);
+        for(int i = 0 ; i  < n ; i++)
+            bw.write(arr[i] + "\n");
+        bw.flush();
+        bw.close();
 
     }
 }
