0f60578b53170df9b70f1b8db1a39fc45a18f933
rong5026 <77156858+rong5026@users.noreply.github.com>
2023-07-28 17:10:07 +0900

    [Gold V] Title: 신기한 소수, Time: 872 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static boolean isPrime(int num) {
+
+        for(int i = 2 ; i <= num / 2 ; i++) {
+            if (num % i == 0)
+                return false;
+        }
+        return true;
+    }
+    public static void DFS(int num, int i, int n) {
+
+        if (i == n){
+            if(isPrime(num))
+                System.out.println(num);
+            return ;
+        }
+        for(int k = 1 ; k < 10 ; k++) {
+            if (isPrime(num * 10 + k))
+                DFS(num * 10 + k, i + 1, n);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        DFS(2,1,n);
+        DFS(3,1,n);
+        DFS(5,1,n);
+        DFS(7,1,n);
+
+    }
+}
