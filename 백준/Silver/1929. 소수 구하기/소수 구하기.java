4aaca74fc6a5f1e88cab88495d2639ff65be8d83
rong5026 <77156858+rong5026@users.noreply.github.com>
2023-08-09 23:14:23 +0900

    [Silver III] Title: 소수 구하기, Time: 2680 ms, Memory: 37036 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int m = input.nextInt();
+
+
+        int size = m - n + 1;
+        int arr[] = new int[size];
+
+        int j = 0;
+        for(int i = n ; i <= m ; i++) {
+            arr[j++] = i;
+        }
+
+        if(arr[0] == 1)
+            arr[0] = -1;
+        for(int i = 2 ; i <=Math.sqrt(m) ; i++) {
+
+            for(int k = 0 ; k < size ; k++) {
+
+                if(arr[k] == -1)
+                    continue;
+                
+                if(arr[k] % i == 0 && arr[k] / i != 1)
+                    arr[k] = -1;
+            }
+        }
+        for(int i = 0 ; i < size ; i++)
+        {
+            if(arr[i] != -1)
+                System.out.println(arr[i]);
+        }
+
+
+    }
+}
