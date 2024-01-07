734c8a601163d82121c6bb54f42eb4692803b671
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2024-01-07 16:03:59 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1712 ms, Memory: 134488 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,12 +1,8 @@
-import javax.swing.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.Buffer;
-import java.sql.Array;
-import java.util.*;
-
-
+import javax.print.DocFlavor;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -18,37 +14,38 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        int [][] arr = new int[n + 1][n + 1];
+        int arr[][] = new int[n + 1][n + 1];
+        int arrSum[][] = new int[n + 1][n + 1];
 
-        for(int i = 1 ; i <= n ; i++) {
+
+        for (int i = 1 ; i <= n ; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 1 ; j <= n ; j++) {
+            for (int j = 1; j <= n ; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
+                arrSum[i][j] = arr[i][j] + arrSum[i][j - 1] + arrSum[i - 1][j] - arrSum[i - 1][j - 1];
             }
         }
 
-        int [][] hong = new int[n + 1][n + 1];
 
-        for(int i = 1 ; i <= n ; i++) {
-            for(int j = 1 ; j <= n ; j++) {
-                hong[i][j] = arr[i][j] + hong[i - 1][j] + hong[i][j - 1] - hong[i - 1][j - 1];
-            }
-        }
-
-        for(int i = 0 ; i < m ; i++) {
-            int [] input = new int[4];
+        for (int i = 0 ; i < m ; i++) {
             st = new StringTokenizer(br.readLine());
+            int [] input = new int[4];
+
             for(int j = 0 ; j < 4 ; j++)
                 input[j] = Integer.parseInt(st.nextToken());
+
             int x1 = input[0];
             int y1 = input[1];
             int x2 = input[2];
             int y2 = input[3];
 
-            int result = hong[x2][y2] - hong[x1 - 1][y2] - hong[x2][y1 - 1] + hong[x1 - 1][y1 -1];
+            int result = arrSum[x2][y2] - arrSum[x2][y1 - 1] - arrSum[x1 - 1][y2] + arrSum[x1 - 1][y1 - 1];
             System.out.println(result);
 
         }
+
+
+
 
     }
 }
