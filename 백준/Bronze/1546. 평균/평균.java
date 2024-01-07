b2af0d1d32cade1fb173d5786d27373baf2996ad
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2024-01-07 15:22:39 +0900

    [Bronze I] Title: 평균, Time: 128 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,31 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.sql.Array;
-import java.util.*;
-
-
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-       int n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-       StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-       long max = 0;
-       long sum = 0;
 
-       for(int i = 0 ; i < n ; i++) {
-           int input = Integer.parseInt(st.nextToken());
-           if(input > max)
-               max = input;
-           sum+=input;
-       }
-       double result = 100.0 * sum / n / max;
+        int sum = 0;
+        int max = -1;
+
+        for(int i = 0 ; i < n ; i++) {
+            int score = Integer.parseInt(st.nextToken());
+            if (score > max) {
+                max = score;
+            }
+            sum += score;
+        }
+
+        double result = 100.0 * sum / n / max;
         System.out.println(result);
     }
 }
