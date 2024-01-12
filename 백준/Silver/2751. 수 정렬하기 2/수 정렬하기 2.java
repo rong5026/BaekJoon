c5c963d960fb88b4c4235cf7b69bc40157d5cf2a
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2024-01-12 14:23:22 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1048 ms, Memory: 112496 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,9 +1,6 @@
-import javax.swing.*;
 import java.io.*;
-import java.nio.Buffer;
-import java.sql.Array;
+import java.lang.reflect.Array;
 import java.util.*;
-
 
 
 public class Main {
@@ -25,37 +22,41 @@ public class Main {
             tmp[k++] = arr[i++];
         while (j <= last)
             tmp[k++] = arr[j++];
-        for(int l = start ; l <=last ; l++)
+        for(int l = start ; l <=last ; l++) {
             arr[l] = tmp[l];
-    }
-    private static void merge_sort(int arr[], int start, int last) {
+        }
 
+//        for(int f = 0 ; f < arr.length ; f++)
+//            System.out.print(arr[f] + " ");
+//        System.out.println();
+    }
+    private static void mergeSort(int arr[], int start, int last) {
         if (last - start < 1)
             return ;
         int mid = (start + last) / 2;
-        merge_sort(arr, start, mid);
-        merge_sort(arr, mid + 1, last);
+        mergeSort(arr, start, mid);
+        mergeSort(arr, mid + 1, last);
         merge(arr, start, last, mid);
     }
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(st.nextToken());
-        int []arr = new int[n];
+        int n = Integer.parseInt(br.readLine());
+
+        int arr[] = new int[n];
         tmp = new int[n];
 
-        for( int i = 0 ; i < n ; i++) {
-            arr[i]= Integer.parseInt(br.readLine());
+        for (int i = 0 ; i < n ; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        merge_sort(arr, 0 , n - 1);
+        mergeSort(arr, 0 , n - 1);
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
         for(int i = 0 ; i  < n ; i++)
             bw.write(arr[i] + "\n");
         bw.flush();
         bw.close();
-
     }
 }
