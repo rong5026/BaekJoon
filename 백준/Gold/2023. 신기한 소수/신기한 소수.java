4ce7b0bea546b9dee996d76caab0cfb03d86f1a5
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2024-01-16 15:36:32 +0900

    [Gold V] Title: 신기한 소수, Time: 788 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,40 +1,42 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.lang.reflect.Array;
 import java.util.*;
+
 
 public class Main {
 
-    public static boolean isPrime(int num) {
+    public static boolean isPrime(int number) {
 
-        for(int i = 2 ; i <= num / 2 ; i++) {
-            if (num % i == 0)
+        for (int i = 2 ; i <= number / 2 ; i++) {
+            if (number % i == 0)
                 return false;
         }
         return true;
     }
-    public static void DFS(int num, int i, int n) {
 
-        if (i == n){
-            if(isPrime(num))
-                System.out.println(num);
-            return ;
+    public static void DFS(int number, int i, int n) {
+
+        if (i == n) {
+            if (isPrime(number))
+                System.out.println(number);
         }
-        for(int k = 1 ; k < 10 ; k++) {
-            if (k % 2 == 0)
-                continue;
-            if (isPrime(num * 10 + k))
-                DFS(num * 10 + k, i + 1, n);
+        else {
+            for (int k = 1 ; k < 10 ; k++) {
+                if (isPrime((number * 10) + k))
+                    DFS((number * 10) + k, i + 1, n);
+            }
         }
     }
-    public static void main(String[] args) throws IOException {
-        Scanner input = new Scanner(System.in);
 
-        int n = input.nextInt();
-        DFS(2,1,n);
-        DFS(3,1,n);
-        DFS(5,1,n);
-        DFS(7,1,n);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        DFS(2, 1, n);
+        DFS(3, 1, n);
+        DFS(5, 1, n);
+        DFS(7, 1, n);
 
     }
 }
