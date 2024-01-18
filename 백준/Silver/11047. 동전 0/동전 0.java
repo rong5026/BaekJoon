6f290270d945ffb40e499cca18a1fa0c76d338de
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2024-01-18 15:35:52 +0900

    [Silver IV] Title: 동전 0, Time: 268 ms, Memory: 17768 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,9 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import org.w3c.dom.Node;
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.nio.Buffer;
 import java.util.*;
 
+
 public class Main {
+
+    public static int getMaxIndex(int arr[], int num) {
+        int start = 0;
+        int last = arr.length - 1;
+
+        int result = 0;
+
+        while (start <= last) {
+            int mid = (start + last) / 2;
+            if (arr[mid]  > num) {
+                last = mid - 1;
+            }
+            else {
+                start = mid + 1;
+                result = mid;
+            }
+        }
+        return result;
+    }
 
     public static void main(String[] args) throws IOException {
 
@@ -13,19 +35,24 @@ public class Main {
         int m = input.nextInt();
 
         int arr[] = new int[n];
-        for(int i = 0 ; i < n ; i++) {
+
+        for (int i = 0 ; i < n ; i++) {
             arr[i] = input.nextInt();
         }
-        int cnt = 0;
-        for(int i = n - 1 ; i >= 0 ; i--) {
 
-            while(arr[i] <= m)
-            {
-                m -= arr[i];
-                cnt++;
+        int index = getMaxIndex(arr, m);
+        int sum = 0;
+        int count = 0;
+
+        for (int i = index ; i >= 0 ; i--) {
+
+            while (sum + arr[i] <= m) {
+                sum += arr[i];
+                count++;
             }
         }
-        System.out.println(cnt);
+
+        System.out.println(count);
 
     }
 }
