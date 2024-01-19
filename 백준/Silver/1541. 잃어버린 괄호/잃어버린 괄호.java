c53f9cd331bd9c9601aec637c186d0aaf33e43df
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2024-01-19 16:09:20 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 124 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,32 +1,53 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import org.w3c.dom.Node;
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.nio.Buffer;
 import java.util.*;
+
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String []line = br.readLine().split("-");
+        String input[] = br.readLine().split("-");
 
         int total = 0;
-        int sum = 0;
-        for(int i = 0 ; i < line.length ; i++) {
+        for (int i = 0 ; i < input.length ; i++) {
 
-            String []arr = line[i].split("\\+");
-            sum = 0;
-            for(int j = 0 ;j < arr.length ; j++) {
+            int sum = 0;
+
+            String arr[] = input[i].split("\\+");
+
+            for (int j = 0 ; j < arr.length ; j++) {
                 sum += Integer.parseInt(arr[j]);
             }
 
-            if(i == 0)
-                total = sum;
+            if (i == 0)
+                total  = sum;
             else
-                total -=sum;
+                total -= sum;
         }
+
         System.out.println(total);
-
-
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
