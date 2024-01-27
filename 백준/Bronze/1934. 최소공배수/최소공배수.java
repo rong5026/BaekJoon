e87a9bdd52852dc0453dc7ed0758c155a52b74fe
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2024-01-27 16:41:20 +0900

    [Bronze I] Title: 최소공배수, Time: 192 ms, Memory: 15064 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -1,31 +1,37 @@
+import org.w3c.dom.Node;
+
 import java.io.*;
+import java.lang.reflect.Array;
+import java.nio.Buffer;
 import java.util.*;
+
 
 public class Main {
 
-    public static int gcd(int a, int b)
-    {
+    private static int GCD(int a, int b) {
+
         if (b == 0)
             return a;
-        return gcd(b , a % b);
+        return GCD(b, a % b);
     }
+
+    private static int LCM(int a, int b) {
+        return a * b / GCD(a,b);
+    }
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
 
-        for(int i = 0 ; i < n ; i++) {
+        for (int i = 0 ; i < n ; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
 
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            int result = a * b / gcd(a, b);
-            System.out.println(result);
-
+            System.out.println(LCM(a,b));
         }
-
-
     }
 }
