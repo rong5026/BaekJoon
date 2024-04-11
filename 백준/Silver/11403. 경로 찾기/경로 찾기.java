26321671ba1b8a8a14edb31337fce1652de844eb
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2024-04-11 16:06:35 +0900

    [Silver I] Title: 경로 찾기, Time: 236 ms, Memory: 20236 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -11,62 +11,43 @@ import java.util.StringTokenizer;
 public class Main {
 
 	static int distance[][];
-	static ArrayList<Integer> list[];
 	static int n;
-	private static void checkVisited(int i, int j) {
 
-		Queue<Integer> queue = new LinkedList<>();
-		boolean visited[] = new boolean[n + 1];
-
-		queue.add(i);
-
-		while (!queue.isEmpty()) {
-			int value = queue.poll();
-			visited[value] = true;
-			for(int elem: list[value]) {
-
-				if (elem == j) {
-					System.out.print("1 ");
-					return;
-				}
-				if (!visited[elem])
-					queue.add(elem);
-			}
-		}
-		System.out.print("0 ");
-	}
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		n = Integer.parseInt(br.readLine());
 
-		list = new ArrayList[n + 1];
 		distance = new int[n + 1][n + 1];
-
-		for (int i = 1 ; i <= n ; i++) {
-			list[i] = new ArrayList<>();
-		}
 
 		for (int i = 1 ; i <= n ; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 1; j <= n; j++) {
-				int value = Integer.parseInt(st.nextToken());
-				if (value == 1) {
-					list[i].add(j);
+				distance[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		for (int k = 1 ; k <= n ; k++) {
+			for (int i = 1 ; i <= n ; i++) {
+				for (int j = 1; j <= n ; j++) {
+					if (distance[i][k] == 1 && distance[k][j] == 1) {
+						distance[i][j] = 1;
+					}
 				}
 			}
 		}
 
 		for (int i = 1 ; i <= n ; i++) {
-			for (int j = 1 ; j <=n ; j++) {
-				checkVisited(i, j);
+			for (int j = 1; j <= n ; j++) {
+				bw.write(distance[i][j] + " ");
 			}
-			System.out.println();
+			bw.write("\n");
 		}
 
-
-
+		bw.flush();
+		bw.close();
 	}
 }
