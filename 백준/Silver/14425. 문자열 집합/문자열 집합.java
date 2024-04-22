ca222eda4bc5742c3ec6b57739cfb1d40a626a90
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2024-04-22 16:36:51 +0900

    [Silver IV] Title: 문자열 집합, Time: 400 ms, Memory: 31720 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -3,11 +3,11 @@ import com.sun.source.tree.Tree;
 import java.io.*;
 import java.util.ArrayList;
 
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	private static ArrayList<String> list;
 
 	public static void main(String[] args) throws IOException {
 
@@ -18,15 +18,12 @@ public class Main {
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 
-		list = new ArrayList<>();
-		// for (int i = 0 ; i < n ; i++) {
-		// 	list[i] = new ArrayList<>();
-		// }
+		HashMap<String, Integer> map = new HashMap<>();
+
 
 		for (int i = 0; i < n ; i++) {
 			String str = br.readLine();
-			// list[i].add(str);
-			list.add(str);
+			map.put(str, 0);
 		}
 
 		int resultCount = 0;
@@ -34,12 +31,9 @@ public class Main {
 		for (int i = 0 ; i < m ; i++) {
 			String input = br.readLine();
 
-			for (int j = 0 ; j < n ; j++) {
-				if (list.get(j).equals(input))
-					resultCount++;
-			}
+			if (map.get(input) != null)
+				resultCount++;
 		}
 		System.out.println(resultCount);
-
 	}
 }
