f4151f804b635e78f43f3d9dbc35d12791374b80
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2024-04-23 16:14:28 +0900

    [Silver I] Title: 트리 순회, Time: 128 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -7,9 +7,21 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
+class hNode {
+	int value;
+	hNode left;
+	hNode right;
+
+	public hNode(int value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+}
+
 public class Main {
 
-	private static int tree[][];
+	private static hNode[] tree;
 
 	public static void main(String[] args) throws IOException {
 
@@ -17,55 +29,61 @@ public class Main {
 
 		int n = Integer.parseInt(br.readLine());
 
-		tree = new int[26][2];
+		tree = new hNode[n + 1];
 
 		for (int i = 0 ; i < n ; i++) {
 
 			String input[] = br.readLine().split(" ");
 
-			int rootIdx = input[0].charAt(0) -'A';
+			char root = input[0].charAt(0);
 			char left = input[1].charAt(0);
 			char right = input[2].charAt(0);
 
-			if (left == '.')
-				tree[rootIdx][0] = -1;
-			else
-				tree[rootIdx][0] = left - 'A';
+			if (root == 'A') {
+				tree[root - 'A'] = new hNode(root - 'A');
+			}
 
-			if (right == '.')
-				tree[rootIdx][1] = -1;
-			else
-				tree[rootIdx][1] = right - 'A';
+			if (left != '.') {
+				tree[left - 'A'] = new hNode(left - 'A');
+				tree[root - 'A'].left = tree[left - 'A'];
+			}
+
+			if (right != '.') {
+				tree[right - 'A'] = new hNode(right - 'A');
+				tree[root - 'A'].right = tree[right - 'A'];
+			}
 		}
 
-		preOrder(0);
+		preOrder(tree[0]);
 		System.out.println();
-		inOrder(0);
+		inOrder(tree[0]);
 		System.out.println();
-		postOrder(0);
+		postOrder(tree[0]);
 		System.out.println();
-	}
-	private static void preOrder(int index) {
-		if (index == -1)
-			return ;
-		System.out.print((char)(index + 'A'));
-		preOrder(tree[index][0]);
-		preOrder(tree[index][1]);
 	}
 
-	private static void inOrder(int index) {
-		if (index == -1)
+	private static void preOrder(hNode node) {
+		if (node == null)
 			return ;
-		inOrder(tree[index][0]);
-		System.out.print((char)(index + 'A'));
-		inOrder(tree[index][1]);
+		System.out.print((char)(node.value + 'A'));
+		preOrder(node.left);
+		preOrder(node.right);
 	}
 
-	private static void postOrder(int index) {
-		if (index == -1)
+	private static void inOrder(hNode node) {
+		if (node == null)
 			return ;
-		postOrder(tree[index][0]);
-		postOrder(tree[index][1]);
-		System.out.print((char)(index + 'A'));
+		inOrder(node.left);
+		System.out.print((char)(node.value + 'A'));
+		inOrder(node.right);
 	}
+
+	private static void postOrder(hNode node) {
+		if (node == null)
+			return ;
+		postOrder(node.left);
+		postOrder(node.right);
+		System.out.print((char)(node.value + 'A'));
+	}
+
 }
