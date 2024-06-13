e785e5739adeacd793c426d2960269670540ae27
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2024-06-13 19:24:52 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1068 ms, Memory: 133008 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,51 +1,44 @@
-import javax.print.DocFlavor;
+import com.sun.source.tree.Tree;
+
 import java.io.*;
-import java.util.Arrays;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
+	public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+		int arr[][] = new int[n + 1][n + 1];
+		int sumArray[][] = new int[n + 1][n + 1];
 
-        int arr[][] = new int[n + 1][n + 1];
-        int arrSum[][] = new int[n + 1][n + 1];
+		// 배열 초기화
+		for (int i = 1 ; i <= n ; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1 ; j <= n ; j++){
+				arr[i][j] = Integer.parseInt(st.nextToken());
+				sumArray[i][j] = arr[i][j] + sumArray[i - 1][j] + sumArray[i][j - 1] - sumArray[i - 1][j - 1];
+			}
+		}
 
+		for (int i = 1 ; i <= m ; i++){
+			String input[] = br.readLine().split(" ");
+			int x1 = Integer.parseInt(input[0]);
+			int y1 = Integer.parseInt(input[1]);
+			int x2 = Integer.parseInt(input[2]);
+			int y2 = Integer.parseInt(input[3]);
+			int result = sumArray[x2][y2] - sumArray[x2][y1 - 1] - sumArray[x1 -1][y2] + sumArray[x1 - 1][y1 - 1];
 
-        for (int i = 1 ; i <= n ; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= n ; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-                arrSum[i][j] = arr[i][j] + arrSum[i][j - 1] + arrSum[i - 1][j] - arrSum[i - 1][j - 1];
-            }
-        }
+			bw.write(result + "\n");
+		}
 
-
-        for (int i = 0 ; i < m ; i++) {
-            st = new StringTokenizer(br.readLine());
-            int [] input = new int[4];
-
-            for(int j = 0 ; j < 4 ; j++)
-                input[j] = Integer.parseInt(st.nextToken());
-
-            int x1 = input[0];
-            int y1 = input[1];
-            int x2 = input[2];
-            int y2 = input[3];
-
-            int result = arrSum[x2][y2] - arrSum[x2][y1 - 1] - arrSum[x1 - 1][y2] + arrSum[x1 - 1][y1 - 1];
-            System.out.println(result);
-
-        }
+		bw.flush();
+		bw.close();
 
 
-
-
-    }
+	}
 }
