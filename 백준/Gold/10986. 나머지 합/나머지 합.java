67db1cd3ab31821f485a14dcb5ea485fa31d6e4f
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2024-06-14 20:36:29 +0900

    [Gold III] Title: 나머지 합, Time: 520 ms, Memory: 120548 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -1,48 +1,43 @@
-import javax.swing.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.Buffer;
-import java.sql.Array;
-import java.util.*;
+import com.sun.source.tree.Tree;
 
-
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) throws IOException {
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        long cnt = 0;
-        long [] sum = new long[n];
-        long [] count = new long[m];
-        st = new StringTokenizer(br.readLine());
+		long sum[] = new long[n + 1];
+		long count[] = new long[m];
+		long resultCnt = 0;
 
-        sum[0] = Long.parseLong(st.nextToken());
+		st = new StringTokenizer(br.readLine());
 
-        for(int i = 1 ; i < n ; i++) {
-            sum[i] = sum[i-1] + Long.parseLong(st.nextToken());
-        }
+		for (int i = 1 ; i <= n ; i++) {
+			long elem = Long.parseLong(st.nextToken());
+			sum[i] = sum[i - 1] + elem;
+		}
 
-        for(int i = 0 ; i < n ; i++) {
-            sum[i] %= m;
-            if(sum[i] == 0)
-                cnt++;
-            count[(int) sum[i]]++;
-        }
+		for (int i = 1; i <= n ; i++) {
+			sum[i] %= m;
 
-        for(int i = 0 ; i < m ; i++) {
-            if(count[i] >= 2) {
-                cnt += count[i] * (count[i] - 1) / 2;
+			if (sum[i] == 0) {
+				resultCnt++;
+			}
+			count[(int)sum[i]]++;
+		}
 
-            }
-        }
+		for (int i = 0 ; i < m ; i++){
+			if (count[i] >=2) {
+				resultCnt += count[i] * (count[i] - 1) / 2;
+			}
+		}
 
-        System.out.println(cnt);
-
-    }
+		System.out.println(resultCnt);
+	}
 }
