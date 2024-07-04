dbc73f4a84b35fc40fdb3456ab21a95cffefa7fa
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2024-07-04 14:12:56 +0900

    [Gold I] Title: K번째 수, Time: 236 ms, Memory: 17744 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.java
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -1,37 +1,44 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.management.MemoryType;
 import java.util.*;
+
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+	private static boolean check(int mid, int n, int m) {
+		int count = 0;
+		for (int i = 1 ; i <= n ; i++) {
+			count += Math.min(mid / i, n);
+		}
+		return count < m;
+	}
+	public static void main(String[] args) throws IOException {
+		Scanner input = new Scanner(System.in);
 
-        Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		int m = input.nextInt();
 
-        int n = input.nextInt();
-        int k = input.nextInt();
+		int start = 1;
+		int last = m;
+		int result = 0;
 
-        long start = 1;
-        long last = k;
-        long result = 0;
+		while (start <= last) {
+			int mid = (start + last) / 2;
 
-        while (start <= last) {
-            long mid = (start + last) / 2;
-            long cnt = 0;
 
-            for(int i = 1 ; i <= n ; i++) {
-                cnt += Math.min(mid/i , n);
-            }
+			if (check(mid, n, m))
+				start = mid + 1;
+			else {
+				result = mid;
+				last = mid - 1;
+			}
+		}
+		System.out.println(result);
 
-            if (cnt < k)
-                start = mid + 1;
-            else {
-                result = mid;
-                last = mid - 1;
-            }
-        }
-        System.out.println(result);
 
-    }
+	}
 }
