d1551e74dd418b91a40bfeb4f806b8c47bd3bc48
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2024-07-14 20:25:14 +0900

    [Silver I] Title: 회의실 배정, Time: 484 ms, Memory: 44980 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,43 +1,44 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.management.MemoryType;
 import java.util.*;
 
+
 public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) throws IOException {
+		int n = Integer.parseInt(br.readLine());
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int arr[][] = new int[n][2];
+		for(int i = 0 ; i < n ; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
 
-        int n = Integer.parseInt(br.readLine());
+		Arrays.sort(arr, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				if (o1[1] == o2[1])
+					return o1[0] - o2[0];
+				return o1[1] - o2[1];
+			}
+		});
 
-        int arr[][]= new int[n][2];
-        for(int i = 0 ; i < n ; i++)
-        {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
-        }
+		int cnt = 0;
+		int time = 0;
 
-        Arrays.sort(arr, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if(o1[1] == o2[1])
-                    return (o1[0] - o2[0]);
-                return (o1[1] - o2[1]);
-            }
-        });
+		for (int i = 0 ; i < n ; i++) {
+			if (time <= arr[i][0]) {
+				time = arr[i][1];
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
 
-        int cnt = 0;
-        int time = 0;
-
-        for(int i = 0; i < n ; i++) {
-            if(time <= arr[i][0]) {
-                time = arr[i][1];
-                cnt++;
-            }
-        }
-
-        System.out.println(cnt);
-    }
+	}
 }
