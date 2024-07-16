cfaf492b4ca3294279fe4e18550702f9f386076a
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2024-07-16 17:26:52 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 240 ms, Memory: 18704 KB -BaekjoonHub

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
@@ -1,37 +1,35 @@
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 
-        Scanner input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 
-        long n = input.nextLong();
-        long m = input.nextLong();
+		long min = input.nextLong();
+		long max = input.nextLong();
+		boolean check[] = new boolean[(int)(max - min + 1)];
+		long cnt = max - min + 1;
 
+		for (long i = 2; i * i <= max ; i++) {
+			long pow = i * i;
+			long startValue = min / pow;
 
-        long cnt =  m - n + 1;
-        boolean arr[] = new boolean[(int)cnt];
+			if (min % pow != 0)
+				startValue++;
 
-
-        for(long i = 2 ; i * i <= m ; i++) {
-            long pow = i *i;
-            long tmp = n / pow;
-
-            if(n % pow != 0)
-                tmp++;
-            for(long j = tmp ; j *pow <= m ; j++) {
-                int index = (int)(j * pow - n);
-                if(!arr[index]) {
-                    arr[index] = true;
-                    cnt--;
-                }
-
-            }
-        }
-        System.out.println(cnt);
+			for (long j = startValue; j * pow <= max; j++) {
+				int index = (int)(j * pow - min); // max의 범위가 min ≤ max ≤ min + 1,000,000 이므로
+				if (!check[index]) {
+					check[index] = true;
+					cnt--;
+				}
+			}
+		}
+		System.out.println(cnt);
 
 
-    }
+
+	}
 }
