bdbf28dbabd5dc4566b163545ea6cde14cff9849
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2024-07-24 20:17:16 +0900

    [Gold IV] Title: 거짓말, Time: 128 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -1,94 +1,91 @@
-import com.sun.source.tree.Tree;
-
-import org.w3c.dom.Node;
-
-import java.io.*;
-import java.nio.Buffer;
-import java.nio.file.LinkPermission;
-import java.sql.Array;
-import java.util.*;
-
-import jdk.jshell.SourceCodeAnalysis;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	static int parent[];
-	static ArrayList<Integer> party[];
-    static ArrayList<Integer> truePerson;
-	public static void union(int a, int b) {
-		a = find(a);
-		b = find(b);
-
-		if (a < b)
-			parent[b] = a;
-		else
-			parent[a] = b;
-	}
-
-	public static int find(int a) {
-		if (parent[a] == a)
+	private static int find(int a) {
+		if (a == parent[a])
 			return a;
 		return parent[a] = find(parent[a]);
 	}
+	private static void union(int a, int b) {
+		a = find(a);
+		b = find(b);
 
-    private static boolean checkPossibleParty(int elem) {
-        for (int k = 0 ; k < truePerson.size() ; k++) {
-            int a = find(truePerson.get(k));
+		if (a > b)
+			parent[a] = b;
+		else
+			parent[b] = a;
+	}
+	private static boolean checkPossibleParty(int elem, int truePerson[]) {
+		for (int i = 0 ; i < truePerson.length ; i++) {
+			int trueParent = find(truePerson[i]);
+			int elemParent = find(elem);
+			if (trueParent == elemParent) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-            if (a == find(elem)){
-               return false;
-            }
-        }
-        return true;
-    }
+	private static int parent[];
+	private static ArrayList<Integer> party[];
 
 	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		Scanner input = new Scanner(System.in);
+		// 첫 줄 입력
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
 
-		int n = input.nextInt();
-		int m = input.nextInt();
-		int trueP = input.nextInt();
-		truePerson = new ArrayList<Integer>();
-
-		for (int i = 0; i < trueP; i++) {
-			truePerson.add(input.nextInt());
-		}
-
+		party = new ArrayList[m + 1];
 		parent = new int[n + 1];
-		for (int i = 1; i <= n; i++) {
+		for (int i = 1 ; i <= n ; i++) {
 			parent[i] = i;
 		}
 
-        party = new ArrayList[m];
 
-		for (int i = 0; i < m; i++) {
-			int inputCount = input.nextInt();
-            party[i] = new ArrayList<Integer>();
+		// 진실 아는 사람 입력
+		st = new StringTokenizer(br.readLine());
+		int trueKnowCnt = Integer.parseInt(st.nextToken());
+		int truePerson[] = new int[trueKnowCnt];
 
-			for (int j = 0; j < inputCount; j++) {
-                party[i].add(input.nextInt());
-				if (j != 0) {
-					union(party[i].get(j - 1),  party[i].get(j));
-				}
+		for (int i = 0 ; i < trueKnowCnt ; i++) {
+			truePerson[i] = Integer.parseInt(st.nextToken());
+		}
+
+		// 각 파티에 참여하는 인원 입력
+		for (int i = 1 ; i <= m ; i++) {
+			st = new StringTokenizer(br.readLine());
+			party[i] = new ArrayList<>();
+
+			int personCnt = Integer.parseInt(st.nextToken());
+
+			for(int j = 0 ; j < personCnt ; j++) {
+				party[i].add(Integer.parseInt(st.nextToken()));
+
+				if (j != 0)
+					union(party[i].get(j - 1), party[i].get(j));
 			}
 		}
 
-        int count = m;
+		int reulstCount = m;
 
-        for (int i = 0 ; i < m ; i++) {
+		for (int i = 1 ; i <= m ; i++) {
 
-            for (int j = 0 ; j < party[i].size() ; j++) {
-                int elem = party[i].get(j);
+			int elem = party[i].get(0);
 
-                if (!checkPossibleParty(elem)) {
-                    count--;
-                    break;
-                }
-            }
-        }
-        System.out.println(count);
+			if(!checkPossibleParty(elem, truePerson)) {
+				reulstCount--;
+			}
+		}
+
+		System.out.println(reulstCount);
+
 	}
-
-
 }
