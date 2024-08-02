67ff44dd41cbbf27ee7a078d2076b3db92a0da18
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2024-08-02 16:51:59 +0900

    [Gold IV] Title: 플로이드, Time: 456 ms, Memory: 43212 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -4,68 +4,57 @@ import java.io.*;
 import java.nio.Buffer;
 import java.util.*;
 
-
-
 public class Main {
-
-	static int n;
-	static int m;
-	static int distance[][];
-
-	static int MAX_VALUE = 100_000_01;
+	static int MAX_VALUE = 100_000_00;
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		n = Integer.parseInt(br.readLine());
-		m = Integer.parseInt(br.readLine());
 
-		distance = new int[n + 1][n + 1];
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+		int map[][] = new int[n][n];
 
-		for (int i = 1 ; i <= n ; i++) {
-			for (int j = 1; j <= n ; j++) {
-
-				if (i == j) {
-					distance[i][j] = 0;
-				}
-				else {
-					distance[i][j] = MAX_VALUE;
-				}
-			}
+		// 2차원배열 초기화
+		for (int i = 0; i < n; i++) {
+			Arrays.fill(map[i], MAX_VALUE);
+		}
+		for (int i = 0 ; i < n ; i++) {
+			map[i][i] = 0;
 		}
 
+		// 입력
 		for (int i = 0 ; i < m ; i++) {
-			String input[] = br.readLine().split(" ");
-			int start =  Integer.parseInt(input[0]);
-			int last = Integer.parseInt(input[1]);
-			int value = Integer.parseInt(input[2]);
+			StringTokenizer st = new StringTokenizer(br.readLine());
 
-			if (distance[start][last] > value)
-				distance[start][last] = value;
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			map[a - 1][b - 1] = Math.min(map[a - 1][b - 1], c);
 		}
 
-		for (int k = 1 ; k <= n ; k++) { // 노드 수
-			for (int i = 1 ; i <= n ; i++) { // 출발 노드
-				for (int j = 1 ; j <= n ; j++) { // 도착 노드
-					if (distance[i][j] > distance[i][k] + distance[k][j]) {
-						distance[i][j] = distance[i][k] + distance[k][j];
+		for (int k = 0 ; k < n ; k++) {
+			for(int i = 0 ; i < n ; i++) {
+				for (int j = 0 ; j < n ; j++) {
+					if (map[i][k] + map[k][j] < map[i][j]) {
+						map[i][j] = map[i][k] + map[k][j];
 					}
 				}
 			}
 		}
 
-		for (int i = 1 ; i <= n ; i++) {
-			for (int j = 1 ; j <= n ; j++){
-				if (distance[i][j] == MAX_VALUE)
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		for(int i = 0 ; i < n ; i++) {
+			for (int j = 0; j < n; j++) {
+				if (map[i][j] == MAX_VALUE)
 					bw.write("0 ");
 				else
-					bw.write(distance[i][j] + " ");
+					bw.write(map[i][j] + " ");
 			}
 			bw.write("\n");
 		}
 
 		bw.flush();
 		bw.close();
-
 	}
 }
+
