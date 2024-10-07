797d4ef00b4c996855c1a19f1137fe4a4cbec5d3
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2024-10-07 16:59:37 +0900

    [Gold III] Title: 나머지 합, Time: 444 ms, Memory: 120660 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -1,4 +1,3 @@
-import com.sun.source.tree.Tree;
 
 import java.io.*;
 import java.util.StringTokenizer;
@@ -6,38 +5,39 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-
-		long sum[] = new long[n + 1];
 		long count[] = new long[m];
-		long resultCnt = 0;
+		long result = 0;
 
 		st = new StringTokenizer(br.readLine());
+		long sum[] = new long[n + 1];
 
 		for (int i = 1 ; i <= n ; i++) {
 			long elem = Long.parseLong(st.nextToken());
 			sum[i] = sum[i - 1] + elem;
-		}
-
-		for (int i = 1; i <= n ; i++) {
 			sum[i] %= m;
-
-			if (sum[i] == 0) {
-				resultCnt++;
-			}
-			count[(int)sum[i]]++;
 		}
 
-		for (int i = 0 ; i < m ; i++){
-			if (count[i] >=2) {
-				resultCnt += count[i] * (count[i] - 1) / 2;
+		for (int i = 1 ; i <= n ; i++) {
+			if (sum[i] == 0) // 구간합 자기 자신일 경우
+				result++;
+			count[(int) sum[i]]++;
+		}
+
+		for (int i = 0 ; i < m ; i++) {
+			if (count[i] >= 2) { // 다른 좌표들에서 nC2 선택하는 경우의 수
+				result += count[i] * (count[i] - 1) / 2;
 			}
 		}
 
-		System.out.println(resultCnt);
+		System.out.println(result);
+
+
 	}
 }
+
+
+
