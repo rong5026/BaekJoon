7ca05aed78e2d82e9eacb504090efdc9262c8bf0
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2024-10-17 15:11:22 +0900

    [Gold IV] Title: 오큰수, Time: 984 ms, Memory: 147860 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,40 +1,49 @@
+import javax.print.DocFlavor;
 import java.io.*;
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int input[] = new int[n];
-		int result[] = new int[n];
+        int n = Integer.parseInt(br.readLine());
 
-		Stack<Integer> stack = new Stack<Integer>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		for (int i = 0 ; i < n ; i++) {
-			result[i] = -1;
-			input[i] = Integer.parseInt(st.nextToken());
+        int arr[] = new int[n];
+        int result[] = new int[n];
 
-			if (stack.isEmpty()) {
-				stack.push(i);
-			}
-			else {
-				while (!stack.isEmpty() && input[i] > input[stack.peek()]) {
-					result[stack.pop()] = input[i];
-				}
-				stack.push(i);
-			}
-		}
+        for (int i = 0 ; i < n ; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Stack<Integer> stack = new Stack<Integer>();
 
-		for (int i = 0 ; i < n ; i++) {
-			sb.append(result[i] + " ");
-		}
+        stack.push(0);
 
-		System.out.println(sb.toString());
-	}
+        for (int i = 1 ; i < n ; i++) {
+
+            while (!stack.isEmpty() && arr[i] > arr[stack.peek()]) {
+
+                int index = stack.pop();
+                result[index] = arr[i];
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            int index = stack.pop();
+            result[index] = -1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0 ; i < n ; i++)
+            sb.append(result[i]).append(" ");
+
+        System.out.println(sb.toString());
+    }
 }
