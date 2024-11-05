682369cf3d771df85072850c40ee876176b655ec
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2024-11-05 16:57:29 +0900

    [Silver IV] Title: 빙고, Time: 176 ms, Memory: 17648 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.java
+++ 백준/Silver/2578. 빙고/빙고.java
@@ -4,73 +4,57 @@ import java.util.Scanner;
 
 public class Main {
 	private static int map[][];
-	private static boolean result[][];
 
 	private static void insertValue(int elem) {
 		for (int i = 0 ; i < 5 ; i++) {
 			for (int j = 0 ; j < 5 ; j++) {
 				if (map[i][j] == elem) {
-					result[i][j] = true;
+					map[i][j] = -1; // 입력된 값 0으로 마킹 처리
 					return;
 				}
 			}
 		}
 	}
 	private static boolean checkBingGo() {
-		int sum =  checkColAndRow() + checkDiag();
-		if (sum >= 3)
-			return true;
-		return false;
-	}
-
-	private static int checkColAndRow() {
 		int bingGoCount = 0;
 
-		for (int i = 0 ; i < 5 ; i++) {
-			int rowCount = 0;
-			int colCount = 0;
-			for (int j = 0 ; j < 5 ; j++) {
-				if (result[j][i] == true) {
-					colCount++;
-				}
-				if (result[i][j] == true) {
-					rowCount++;
-				}
-			}
-			if (rowCount == 5) {
-				bingGoCount++;
-			}
-			if (colCount == 5) {
-				bingGoCount++;
-			}
-
-		}
-		return bingGoCount;
-	}
-
-	private static int checkDiag() {
-		int bingGoCount = 0;
 		int leftToRight = 0;
 		int rightToLeft = 0;
+		int rowCount = 0;
+		int colCount = 0;
+
 		for (int i = 0 ; i < 5 ; i++) {
-			if (result[i][i]) {
-				leftToRight++;
+			rowCount = 0;
+			colCount = 0;
+			for (int j = 0 ; j < 5 ; j++) {
+				rowCount += map[i][j];
+				colCount += map[j][i];
+
+				if (i == j) {
+					leftToRight += map[i][j];
+				}
+				if (j == 4 - i) {
+					rightToLeft += map[i][j];
+				}
 			}
-			if (result[i][4 - i]) {
-				rightToLeft++;
-			}
+			if (rowCount == -5)
+				bingGoCount++;
+			if (colCount == -5)
+				bingGoCount++;
+			if (leftToRight == -5)
+				bingGoCount++;
+			if (rightToLeft == -5)
+				bingGoCount++;
 		}
-		if (leftToRight == 5)
-			bingGoCount++;
-		if (rightToLeft == 5)
-			bingGoCount++;
-		return bingGoCount;
+
+		if (bingGoCount >= 3)
+			return true;
+		return false;
 	}
 
 	public static void main(String[] args) throws IOException {
 		Scanner input = new Scanner(System.in);
 		map = new int[5][5];
-		result = new boolean[5][5];
 
 		// 입력
 		for (int i = 0 ; i < 5 ; i++) {
