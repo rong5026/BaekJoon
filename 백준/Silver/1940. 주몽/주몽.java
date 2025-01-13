8e4ba1465db8507d1a8b8fb7e5c4163d91ccbb92
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2025-01-13 11:27:55 +0900

    [Silver IV] Title: 주몽, Time: 168 ms, Memory: 16432 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,46 +1,52 @@
-import javax.swing.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.Buffer;
-import java.sql.Array;
-import java.util.*;
 
-
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+		int input[] = new int[n];
+		int start = 0;
+		int last = n - 1;
+		int count = 0;
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int [] array = new int[n];
+		for (int i = 0 ; i < n ; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+		Arrays.sort(input);
 
-        for(int i = 0 ; i < n ; i++) {
-            array[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(array);
+		while (start < last) {
+			int elem = input[start] + input[last];
 
-        int start = 0;
-        int last = n - 1;
-        int cnt = 0;
+			if (elem > m) {
+				last--;
+			}
+			else if (elem < m){
+				start++;
+			}
+			else {
+				count++;
+				start++;
+				last--;
+			}
+		}
+		System.out.println(count);
 
-        while(start < last) {
-
-            int sum = array[start] + array[last];
-            if(sum == m) {
-                cnt++;
-                start++;
-            }
-            else if(sum < m)
-                start++;
-            else
-                last--;
-        }
-        System.out.println(cnt);
-    }
+	}
 }
+
+
+
