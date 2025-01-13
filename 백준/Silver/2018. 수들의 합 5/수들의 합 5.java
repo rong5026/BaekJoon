52b30db979b4554e93ea5f674f2412a8aec6f89d
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2025-01-13 11:18:16 +0900

    [Silver V] Title: 수들의 합 5, Time: 136 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -1,36 +1,42 @@
-import javax.print.DocFlavor;
+
 import java.io.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int start = 1;
+		int last = 1;
+		int sum = 1;
+		int count = 0;
 
-       int n = Integer.parseInt(br.readLine());
+		while (last <= n) {
+			if (sum <= n) {
+				if (sum == n) {
+					count++;
+				}
+				last++;
+				sum += last;
+			}
+			else  {
+				sum -= start;
+				start++;
+			}
 
-       int start = 1;
-       int last = 1;
-       long sum = 1;
-       int count = 1;
-
-       while (last != n) {
-
-           if (sum <= n) {
-               if (sum == n) {
-                   count++;
-               }
-               last++;
-               sum += last;
-           }
-           else {
-               sum -= start;
-               start++;
-           }
-       }
-        System.out.println(count);
-    }
+		}
+		System.out.println(count);
+	}
 }
+
+
+
