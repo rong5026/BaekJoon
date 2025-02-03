61ec4c387cc8435706acf853aec0d99ffa71d7dd
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2025-02-03 19:24:16 +0900

    [Silver V] Title: K번째 수, Time: 1880 ms, Memory: 375964 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.java
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -1,32 +1,32 @@
+
 import java.io.*;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+import java.sql.PreparedStatement;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-
-		List<Long> list = new ArrayList<>();
+		int input[] = new int[n];
 
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0 ; i < n ; i++) {
-			list.add(Long.parseLong(st.nextToken()));
+		for (int i = 0; i < n; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
 		}
 
-		Collections.sort(list);
+		Arrays.sort(input);
 
-		System.out.println(list.get(m - 1));
+		System.out.println(input[m - 1]);
 
 	}
 }
+
+
+
