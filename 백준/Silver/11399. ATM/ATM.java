6ac8ca1400ecb5159fe0c1f9a059bbdbd226c3df
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2025-02-03 19:08:29 +0900

    [Silver IV] Title: ATM, Time: 116 ms, Memory: 14580 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,39 +1,35 @@
+
 import java.io.*;
-import java.lang.reflect.Array;
+import java.sql.PreparedStatement;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
-
 public class Main {
-    public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int input[] = new int[n];
 
-        int arr[] = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(input);
 
-        for (int i = 0 ; i < n ; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
+		int []sum = new int[n + 1];
+		int result = 0;
 
-        Arrays.sort(arr);
+		for (int i = 1; i <=n ; i++) {
+			sum[i] = sum[i-1] + input[i - 1];
+			result += sum[i];
+		}
 
-        int sumArr[] = new int[n];
-
-        sumArr[0] = arr[0];
-        for (int i = 1; i < n ; i++) {
-            sumArr[i] = arr[i] + sumArr[i - 1];
-        }
-
-        int resultSum = 0;
-
-        for (int i = 0 ; i < n ; i++) {
-            resultSum += sumArr[i];
-        }
-
-        System.out.println(resultSum);
-
-    }
+		System.out.println(result);
+	}
 }
+
+
+
