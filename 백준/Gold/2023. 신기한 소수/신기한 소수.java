2915103f96a07424c5187d8f341e604a94e39c76
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2025-02-09 15:46:28 +0900

    [Gold V] Title: 신기한 소수, Time: 100 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,40 +1,47 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.*;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
+	private static int n;
+
 	private static boolean isPrime(int num) {
-		for (int i = 2 ; i < num /2  ; i++) {
-			if (num % i == 0)
+		if (num < 2) {
+			return false;
+		}
+		for (int i = 2 ; i <= Math.sqrt(num) ; i++) {
+			if (num % i == 0) {
 				return false;
+			}
 		}
 		return true;
 	}
-	private static void DFS(int num, int i, int n) {
 
-		if (i == n) {
-			System.out.println(num);
+	private static void DFS(int elem, int jari) {
+		if (jari == n) {
+			if (isPrime(elem))
+				System.out.println(elem);
 			return;
 		}
-		for (int j = 0 ; j <= 4 ; j++) {
-			int elem = 2 * j + 1;
-			if (isPrime(num * 10 + elem))
-				DFS(num * 10 + elem, i + 1, n);
+
+		for (int i = 1 ; i < 10 ; i += 2) {
+			if (isPrime(elem)) {
+				DFS(elem * 10 + i, jari + 1);
+			}
 		}
 	}
+
 	public static void main(String[] args) throws IOException {
-		Scanner input = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int n = input.nextInt();
+		n = Integer.parseInt(br.readLine());
+		// 2, 3, 5, 7
+		// 1, 3, 5, 7 반복
 
-		DFS(2, 1, n);
-		DFS(3, 1, n);
-		DFS(5, 1, n);
-		DFS(7, 1, n);
-
+		DFS(2, 1);
+		DFS(3, 1);
+		DFS(5, 1);
+		DFS(7, 1);
 	}
 }
