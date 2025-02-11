edc4d6a56da8433787e8ba3ece720728ea8d42c6
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2025-02-11 14:40:43 +0900

    [Silver I] Title: 기타 레슨, Time: 296 ms, Memory: 24836 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.java
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -1,101 +1,85 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.*;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 
 public class Main {
 
-	private static int checkSeperatedCount(int input[], int valid) {
+	private static int n,m;
 
-		int sum = 0;
-		int count = 0;
+	private static int getBlueLayCount(int[] sum, int blueLaySize) {
+		int blueLayCnt = 1;
+		int lastBlueLayValue = 0;
 
-		for (int i = 0 ; i < input.length ; i++) {
-			if (sum + input[i] < valid) {
-				sum += input[i];
-			}
-			else {
-				if (i != input.length - 1) {
-					sum = 0;
-					count++;
-					i--;
-				}
-			}
-		}
-		return count;
-	}
-	public static int findBlueLay(int arr[], int value) {
-
-
-		int count = 0;
-		int sum = 0;
-		for(int i = 0 ; i < arr.length ; i++) {
-
-			if( sum + arr[i] <= value) {
-				sum += arr[i];
-				if( i == arr.length - 1)
-					count++;
-			}
-			else {
-				count++;
-				sum = 0;
+		for (int i = 1; i <= n; i++) {
+			if (sum[i] - lastBlueLayValue > blueLaySize) {
+				blueLayCnt++;
+				lastBlueLayValue = sum[i - 1];
 				i--;
 			}
 		}
-		return count;
+		return blueLayCnt;
 	}
-	private static int binarySearch(int input[], int sum, int max, int m) {
+
+	private static int getMinBLueLay(int[] sum, int total, int max) {
 
 		int start = max;
-		int last = sum;
-		int result = 0;
+		int last = total;
+		int result = total;
 
 		while (start <= last) {
-			int mid = (start + last) /2;
+			int mid = (start + last) / 2;
+			int bluelayCount = getBlueLayCount(sum, mid);
 
-			int seperatedCount = findBlueLay(input, mid);
-			if (start == last && last == mid && result == 0)
+			// System.out.println("start :" + start + " mid :" + mid + " last :" + last + " bluelayCount :" + bluelayCount);
+			if (start == mid && mid == last && result == total)
 				result = mid;
 
-			if (seperatedCount > m) {
-				start = mid + 1;
-			}
-			else if (seperatedCount < m) {
+			if (bluelayCount < m) {
 				last = mid - 1;
 			}
+			else if (bluelayCount > m) {
+				start = mid + 1;
+			}
 			else {
-				result = mid;
+				if (result > mid)
+					result = mid;
 				last = mid - 1;
 			}
 		}
 		return result;
+
 	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		int input[] = new int[n];
-
+		n =  Integer.parseInt(st.nextToken());
+		m =  Integer.parseInt(st.nextToken());
 		int max = 0;
-		int sum = 0;
+
 		st = new StringTokenizer(br.readLine());
+		int[] input = new int[n + 1];
+		int[] sum = new int[n + 1];
+		int total = 0;
 
-		for (int i = 0 ; i < n ; i++) {
-			input[i] = Integer.parseInt(st.nextToken());
-
+		for (int i = 1 ; i <= n ; i++) {
+			input[i] =  Integer.parseInt(st.nextToken());
+			total += input[i];
+			sum[i] = sum[i - 1] + input[i];
 			if (input[i] > max) {
 				max = input[i];
 			}
-			sum += input[i];
 		}
 
-		System.out.println(binarySearch(input, sum, max, m));
-
-
+		System.out.println(getMinBLueLay(sum , total, max));
+		// System.out.println(getBlueLayCount(sum, 17));
 	}
 }
