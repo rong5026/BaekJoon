03d6ab6057b1f95d849d176079add196d10d932b
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2025-02-13 16:00:23 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 332 ms, Memory: 26380 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -1,31 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+
+import java.io.*;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
 
-     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-     int n = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> queue = new PriorityQueue<>();
 
-     PriorityQueue<Integer> queue = new PriorityQueue<>();
+		for (int i = 0; i < n; i++) {
+			int elem = Integer.parseInt(br.readLine());
+			queue.add(elem);
+		}
 
-     for(int i = 0; i < n ; i++) {
-         queue.add(Integer.parseInt(br.readLine()));
-     }
+		int sum = 0;
+		while (queue.size() != 1) {
+			int a = queue.poll();
+			int b = queue.poll();
+			sum += a + b;
+			queue.add(a + b);
+		}
+		System.out.println(sum);
 
-     int sum = 0;
-     while(queue.size() != 1 )
-     {
-         int a = queue.poll();
-         int b = queue.poll();
-         sum += a  + b;
-         queue.add(a + b);
-     }
-     System.out.println(sum);
-
-    }
+	}
 }
