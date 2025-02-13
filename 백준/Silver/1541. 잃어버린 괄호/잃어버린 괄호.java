b93e81488e8ff451bca37f332c8df493183b44b6
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2025-02-13 19:40:44 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 100 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,39 +1,35 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.lang.management.MemoryType;
-import java.util.*;
+
+import java.io.*;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
 
 
 public class Main {
-	public static void main(String[] args) throws IOException {
 
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		String input[] = br.readLine().split("-");
-		int result = 0;
+		String []input = br.readLine().split("-");
+		int total = 0;
 
-		for (int i = 0 ; i < input.length ; i++) {
-
-			String input2[] = input[i].split("\\+");
-
+		for (int i = 0; i < input.length; i++) {
+			String []elems = input[i].split("\\+");
 			int sum = 0;
-			for (int j = 0 ; j < input2.length ; j++) {
-				sum += Integer.parseInt(input2[j]);
+			for (String elem : elems) {
+				sum += Integer.valueOf(elem);
 			}
-
-			if (i == 0) {
-				result += sum;
+			if (i != 0) {
+				total -= sum;
 			}
 			else {
-				result -= sum;
+				total += sum;
 			}
 		}
-		System.out.println(result);
-
-
+		System.out.println(total);
 
 
 	}
