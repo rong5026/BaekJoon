dfd41aab5738aa14ddd82d5c3a0bac89bafe4b01
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2025-02-18 21:54:09 +0900

    [Silver III] Title: 소수 구하기, Time: 676 ms, Memory: 33620 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,32 +1,42 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
- 
+
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
+
+	private static boolean isPrime(int n) {
+		if (n < 2)
+			return false;
+
+		for (int i = 2 ; i <= Math.sqrt(n); i++) {
+			if (n % i == 0)
+				return false;
+		}
+		return true;
+	}
+
 	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        int M = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
-        
-        boolean[] prime = new boolean[N + 1];
-        
-        for(int i = 2; i <= N; i++) {
-        	if(prime[i]) continue;
-        	
-        	if(i >= M) sb.append(i).append('\n');
-        	
-        	for(int j = i + i; j <= N; j += i) {
-        		prime[j] = true;
-        	}
-        }
-        
-        System.out.println(sb);
-    }
- 
+		Scanner input = new Scanner(System.in);
+
+		int n = input.nextInt();
+		int m = input.nextInt();
+
+		boolean[] array = new boolean[m + 1];
+		for (int i = 2 ; i <= Math.sqrt(m) ; i++) {
+			if (array[i])
+				continue;
+			for (int j = i + i ; j <= m ; j = j + i ) {
+				array[j] = true;
+			}
+		}
+
+		for (int i = n; i <= m ; i++) {
+			if (!array[i] && i > 1) {
+				System.out.println(i);
+			}
+		}
+
+
+
+	}
 }
- 
