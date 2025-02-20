fc5319b6526dd02150e41c01746a04689b3e00c9
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2025-02-20 20:36:31 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 268 ms, Memory: 78168 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
@@ -1,40 +1,51 @@
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
 
-	private static boolean isPrime(int num) {
-		if (num == 1)
+	private static boolean isPrime(int n) {
+		if (n < 2)
 			return false;
-		for (int i = 2 ; i <= Math.sqrt(num) ; i++) {
-			if (num % i == 0)
+
+		for (int i = 2; i <= Math.sqrt(n); i++) {
+			if (n % i == 0)
 				return false;
 		}
 		return true;
 	}
-	private static boolean checkPerim(int num) {
-		char array[] = String.valueOf(num).toCharArray();
 
+	private static boolean isPerindrum(int n) {
+
+		char[] array = String.valueOf(n).toCharArray();
 		for (int i = 0 ; i < array.length / 2 ; i++) {
-			if (array[i] != array[array.length -1 -i])
+			if (array[i] != array[array.length - 1 - i])
 				return false;
 		}
 		return true;
 	}
+
 	public static void main(String[] args) throws IOException {
-		Scanner input = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int n = input.nextInt();
-
-		while(true) {
-			if (checkPerim(n) && isPrime(n))
-				break;
-			else
-				n++;
+		int n = Integer.parseInt(br.readLine());
+		boolean[] input = new boolean[10000001];
+		input[1] = true;
+		for (int i = 2 ; i < Math.sqrt(input.length) ; i++) {
+			if (input[i])
+				continue;
+			for (int j = i + i ; j < input.length; j += i ) {
+				input[j] = true;
+			}
 		}
-		System.out.println(n);
+
+		while (true) {
+			if (isPerindrum(n) && !input[n]) {
+				System.out.println(n);
+				break;
+			}
+			n++;
+		}
 
 	}
 }
