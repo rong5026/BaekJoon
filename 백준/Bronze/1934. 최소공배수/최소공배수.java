3b3242b45be31f3eeb139b09c2a04cbd62e075ff
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2025-02-24 17:34:51 +0900

    [Bronze I] Title: 최소공배수, Time: 148 ms, Memory: 14936 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -1,37 +1,31 @@
-import org.w3c.dom.Node;
 
 import java.io.*;
-import java.lang.reflect.Array;
-import java.nio.Buffer;
-import java.util.*;
-
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    private static int GCD(int a, int b) {
 
-        if (b == 0)
-            return a;
-        return GCD(b, a % b);
-    }
+	private static int gcd(int a, int b) {
 
-    private static int LCM(int a, int b) {
-        return a * b / GCD(a,b);
-    }
+		if (b == 0)
+			return a;
+		return gcd(b, a % b);
+	}
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
 
-        int n = Integer.parseInt(br.readLine());
+		for (int i = 0 ; i < n ; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
 
-        for (int i = 0 ; i < n ; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+			System.out.println(a * b / gcd(a, b));
+		}
 
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
 
-            System.out.println(LCM(a,b));
-        }
-    }
+	}
 }
