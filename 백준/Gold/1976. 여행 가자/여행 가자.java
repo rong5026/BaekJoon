13c18bd791c873ad3d35a2d44858ad5b04359240
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2025-02-26 15:23:13 +0900

    [Gold IV] Title: 여행 가자, Time: 144 ms, Memory: 16352 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -1,71 +1,69 @@
-import com.sun.source.tree.Tree;
-import org.w3c.dom.Node;
 
 import java.io.*;
-import java.nio.Buffer;
-import java.nio.file.LinkPermission;
-import java.sql.Array;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
+	private static int[] parent;
 
-    public static void union(int i, int j, int parent[]) {
-        i = find(i, parent);
-        j = find(j, parent);
+	private static int find(int a) {
+		if (a == parent[a])
+			return a;
+		return parent[a] = find(parent[a]);
+	}
 
-        if (i < j)
-            parent[j] = i;
-        else
-            parent[i] = j;
-    }
+	private static void union(int a, int b) {
+		a = find(a);
+		b = find(b);
 
-    public static int find(int num, int parent[]) {
-        if (num == parent[num])
-            return num;
-        return parent[num] = find(parent[num], parent);
-    }
+		if (a < b) {
+			parent[b] = a;
+		}
+		else {
+			parent[a] = b;
+		}
+	}
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(br.readLine());
 
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
+		parent = new int[n + 1];
 
-        int map[][] = new int[n + 1][n + 1];
-        int input[] = new int[m + 1];
-        int parent[] = new int[n + 1];
+		for (int i = 1 ; i <= n ; i++) {
+			parent[i] = i;
+		}
 
-        for (int i = 1 ; i <= n ; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 1 ; j <= n ; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+		for (int i = 1 ; i <= n ; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1 ; j <= i - 1 ; j++) {
+				int elem = Integer.parseInt(st.nextToken());
+				if (elem == 1) {
+					union(i, j);
+				}
+			}
+		}
 
-        for (int i = 1 ; i <= n ; i++) {
-            parent[i] = i;
-        }
+		st = new StringTokenizer(br.readLine());
+		int start = 0;
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0 ; i < m ; i++) {
+			int elem = Integer.parseInt(st.nextToken());
 
-        for (int i = 1 ; i <= m ; i++) {
-            input[i] = Integer.parseInt(st.nextToken());
-        }
+			if (i == 0)
+				start = find(elem);
 
-        for (int i = 1 ; i <= n ; i++) {
-            for (int j = 1; j <= n ; j++) {
-                if (map[i][j] == 1)
-                    union(i, j, parent);
-            }
-        }
-
-        for (int i = 1 ; i <= m ; i++){
-            if (parent[input[i]] != parent[input[1]]) {
-                System.out.println("NO");
-                return ;
-            }
-        }
-        System.out.println("YES");
-    }
+			if (start != find(elem)) {
+				System.out.println("NO");
+				return;
+			}
+		}
+		System.out.println("YES");
+	}
 }
