0b09a808b54ef9561368b7b8c0fb9efb65759711
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2025-02-26 17:46:22 +0900

    [Gold IV] Title: 거짓말, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -1,91 +1,105 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+	private static int[] parent;
 
 	private static int find(int a) {
 		if (a == parent[a])
 			return a;
 		return parent[a] = find(parent[a]);
 	}
+
 	private static void union(int a, int b) {
 		a = find(a);
 		b = find(b);
 
-		if (a > b)
-			parent[a] = b;
-		else
+		if (a < b) {
 			parent[b] = a;
-	}
-	private static boolean checkPossibleParty(int elem, int truePerson[]) {
-		for (int i = 0 ; i < truePerson.length ; i++) {
-			int trueParent = find(truePerson[i]);
-			int elemParent = find(elem);
-			if (trueParent == elemParent) {
-				return false;
-			}
 		}
-		return true;
+		else {
+			parent[a] = b;
+		}
 	}
-
-	private static int parent[];
-	private static ArrayList<Integer> party[];
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		// 첫 줄 입력
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 
-		party = new ArrayList[m + 1];
+
+		// parent 초기화
 		parent = new int[n + 1];
-		for (int i = 1 ; i <= n ; i++) {
+		for (int i = 1 ; i <=n ; i++) {
 			parent[i] = i;
 		}
 
-
-		// 진실 아는 사람 입력
 		st = new StringTokenizer(br.readLine());
-		int trueKnowCnt = Integer.parseInt(st.nextToken());
-		int truePerson[] = new int[trueKnowCnt];
+		int knowCnt = Integer.parseInt(st.nextToken());
+		int[] knowPeople = new int[knowCnt + 1];
 
-		for (int i = 0 ; i < trueKnowCnt ; i++) {
-			truePerson[i] = Integer.parseInt(st.nextToken());
+		if (knowCnt == 0) {
+			System.out.println(m);
+			return;
 		}
 
-		// 각 파티에 참여하는 인원 입력
+		int prev = 1;
+		// 진실을 알고 있는 사람
+		for (int i = 1 ; i <= knowCnt ; i++) {
+			knowPeople[i]= Integer.parseInt(st.nextToken());
+			if (i == 1)
+				prev = knowPeople[i];
+			union(prev , knowPeople[i]);
+			prev = knowPeople[i];
+		}
+
+
+		prev = Integer.MAX_VALUE;
+
+		ArrayList<Integer> partyValue[] = new ArrayList[m + 1];
+
+		for (int i = 1; i <= m ; i++) {
+			partyValue[i] = new ArrayList<>();
+		}
+
+		// 파티 input
 		for (int i = 1 ; i <= m ; i++) {
 			st = new StringTokenizer(br.readLine());
-			party[i] = new ArrayList<>();
+			int partyPerson = Integer.parseInt(st.nextToken());
 
-			int personCnt = Integer.parseInt(st.nextToken());
+			for (int j = 1 ; j <= partyPerson ; j++) {
+				int elem = Integer.parseInt(st.nextToken());
+				partyValue[i].add(elem);
 
-			for(int j = 0 ; j < personCnt ; j++) {
-				party[i].add(Integer.parseInt(st.nextToken()));
-
-				if (j != 0)
-					union(party[i].get(j - 1), party[i].get(j));
+				if (j == 1) {
+					prev = elem;
+				}
+				union(prev, elem);
+				prev = elem;
 			}
 		}
 
-		int reulstCount = m;
+		int total = m;
+		int start = knowPeople[1];
 
-		for (int i = 1 ; i <= m ; i++) {
+		// 파티중 거짓가능한 수 파악
+		for (int i = 1; i <= m ; i++) {
 
-			int elem = party[i].get(0);
-
-			if(!checkPossibleParty(elem, truePerson)) {
-				reulstCount--;
+			for (int elem : partyValue[i]) {
+				if (find(start) == find(elem)) {
+					total--;
+					break;
+				}
 			}
 		}
 
-		System.out.println(reulstCount);
-
+		System.out.println(total);
 	}
 }
