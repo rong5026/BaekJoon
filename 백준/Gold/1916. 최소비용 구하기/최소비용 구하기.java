981bf497a5f4f3445950d380096de8a7ec01d700
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2025-03-04 20:46:30 +0900

    [Gold V] Title: 최소비용 구하기, Time: 436 ms, Memory: 50572 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,76 +1,85 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-class hNode {
+class hNode implements Comparable<hNode>{
 	int node, value;
 
-	public hNode(int node, int value) {
-		this.node = node;
+	public hNode (int node, int value) {
 		this.value = value;
+		this.node = node;
+	}
+
+	@Override
+	public int compareTo(hNode o) {
+		return this.value - o.value;
 	}
 }
+
 public class Main {
+	private static ArrayList<hNode> list[];
+	private static int[] distance;
+	private static boolean[] visited;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int n = Integer.parseInt(br.readLine());
 		int m = Integer.parseInt(br.readLine());
-		ArrayList<hNode> list[] = new ArrayList[n + 1];
-		StringTokenizer st;
+
+		list = new ArrayList[n + 1];
+		distance = new int[n + 1];
+		visited = new boolean[n + 1];
+
 		for (int i = 1; i <= n ; i++) {
 			list[i] = new ArrayList<>();
 		}
 
-		for (int i = 0; i < m ; i++) {
-			st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= m ; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			int value = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
 
-			list[a].add(new hNode(b, value));
+			list[a].add(new hNode(b, c));
 		}
 
-		st = new StringTokenizer(br.readLine());
-
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		int start = Integer.parseInt(st.nextToken());
 		int last = Integer.parseInt(st.nextToken());
 
-		int price[] = new int[n + 1];
-
-		for (int i = 1; i <= n ; i++) {
-			if (start == i)
-				price[i] = 0;
-			else
-				price[i] = Integer.MAX_VALUE;
+		for (int i = 1 ; i <= n ; i++) {
+			if (start != i) {
+				distance[i] = Integer.MAX_VALUE;
+			}
 		}
 
-		PriorityQueue<hNode> queue = new PriorityQueue<>(Comparator.comparingInt(hNode->hNode.value));
-		boolean visited[] = new boolean[n + 1];
-
+		PriorityQueue<hNode> queue = new PriorityQueue<>();
 		queue.add(new hNode(start, 0));
 
 		while (!queue.isEmpty()) {
-			hNode now = queue.poll();
-			if (!visited[now.node]){
-				visited[now.node] = true;
+			hNode elem = queue.poll();
 
-				for (hNode next : list[now.node]) {
-					if (!visited[next.node] && price[next.node] > price[now.node] + next.value) {
-						price[next.node] = price[now.node] + next.value;
-						queue.add(new hNode(next.node, price[next.node]));
-					}
-				}
+			if (visited[elem.node]) {
+				continue;
 			}
 
+			visited[elem.node] = true;
+
+			for (hNode next: list[elem.node]) {
+				if (distance[next.node] > distance[elem.node] + next.value) {
+					distance[next.node] = distance[elem.node] + next.value;
+					queue.add(new hNode(next.node, distance[next.node]));
+				}
+			}
 		}
-		System.out.println(price[last]);
+
+		System.out.println(distance[last]);
 	}
 }
