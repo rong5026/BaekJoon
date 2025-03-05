afa515279696221bf84fbfa00d14b1e747ce6f4f
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2025-03-05 16:15:31 +0900

    [Platinum V] Title: 오민식의 고민, Time: 108 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Platinum/1219. 오민식의 고민/오민식의 고민.java
+++ 백준/Platinum/1219. 오민식의 고민/오민식의 고민.java
@@ -1,131 +1,120 @@
-import com.sun.source.tree.Tree;
 
 import java.io.*;
-import java.nio.Buffer;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 class Edge {
-	int start;
-	int last;
-	int value;
+	int start,last,price;
 
-	public Edge(int start, int last, int value) {
+	public Edge(int start, int last, int price) {
 		this.start = start;
 		this.last = last;
-		this.value = value;
+		this.price = price;
 	}
 }
 
+
 public class Main {
 
-	static long distance[];
-	static Edge edges[];
-	static long price[];
-	static boolean visited[];
-
-	private static void bfs(ArrayList<Integer> list[], int endCity) {
-
+	private static void bfs(int start, ArrayList<Integer>[] list, boolean[] visited) {
 		Queue<Integer> queue = new LinkedList<>();
-
-		queue.add(endCity);
+		queue.add(start);
 
 		while (!queue.isEmpty()) {
+			int now = queue.poll();
+			visited[now] = true;
 
-			int next = queue.poll();
-			visited[next] = true;
-
-			for (int elem : list[next]) {
-				if (!visited[elem]) {
-					queue.add(elem);
+			for (int next: list[now]) {
+				if (!visited[next]) {
+					queue.add(next);
 				}
 			}
 		}
 	}
 
 	public static void main(String[] args) throws IOException {
-
-		// 초기값
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		int n = Integer.parseInt(st.nextToken());
-		int startCity = Integer.parseInt(st.nextToken());
-		int lastCity = Integer.parseInt(st.nextToken());
+		int start = Integer.parseInt(st.nextToken());
+		int last = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 
-		distance = new long[n];
-		edges = new Edge[m];
-		price = new long[n];
-		visited = new boolean[n];
-		ArrayList<Integer> checkRecycleList = new ArrayList<>();
 		ArrayList<Integer> list[] = new ArrayList[n];
+		ArrayList<Integer> check = new ArrayList<>();
+		boolean visited[] = new boolean[n];
 
-		Arrays.fill(distance, Integer.MIN_VALUE);
-
+		Edge[] edges = new Edge[m + 1];
+		long[] prices = new long[n];
+		long[] pay = new long[n];
 
 		for (int i = 0 ; i < n ; i++) {
+			if (i != start) {
+				prices[i] = Integer.MIN_VALUE;
+			}
 			list[i] = new ArrayList<>();
 		}
 
-		for (int i = 0; i < m; i++) {
+		for (int i = 0 ; i < m ; i++) {
 			st = new StringTokenizer(br.readLine());
 
-			int start = Integer.parseInt(st.nextToken());
-			int last = Integer.parseInt(st.nextToken());
-			int value = Integer.parseInt(st.nextToken()) * (-1);
+			int s = Integer.parseInt(st.nextToken());
+			int l = Integer.parseInt(st.nextToken());
+			int price = Integer.parseInt(st.nextToken());
 
-			list[start].add(last);
-			edges[i] = new Edge(start, last, value);
+			edges[i] = new Edge(s, l, price);
+			list[s].add(l);
 		}
 
+		// 돈벌이
 		st = new StringTokenizer(br.readLine());
-
-		for (int i = 0; i < n; i++) {
-			price[i] = Integer.parseInt(st.nextToken());
+		for (int i = 0 ; i <n ; i++) {
+			pay[i] = Integer.parseInt(st.nextToken());
 		}
-		distance[startCity] = price[startCity];
 
-
-		// 비용 계산
-		for (int i = 0; i < m; i++) {
-			Edge edge = edges[i];
-			edge.value += price[edge.last];
-		}
+		prices[start] = pay[start];
 
 		for (int i = 1; i < n ; i++) {
-			for (int j = 0; j < m; j++) {
+			for (int j = 0 ; j < m ; j++) {
 				Edge edge = edges[j];
 
-				if (distance[edge.start] != Integer.MIN_VALUE
-					&& distance[edge.last] < edge.value + distance[edge.start]) {
-					distance[edge.last] = edge.value + distance[edge.start];
+				if (prices[edge.start] != Integer.MIN_VALUE &&
+				prices[edge.last] < prices[edge.start] - edge.price + pay[edge.last]) {
+					prices[edge.last] = prices[edge.start] - edge.price + pay[edge.last];
 				}
 			}
 		}
 
-		for (int j = 0; j < m; j++) {
+		for (int j = 0 ; j < m ; j++) {
 			Edge edge = edges[j];
-			if (distance[edge.start] != Integer.MIN_VALUE
-				&& distance[edge.last] < edge.value + distance[edge.start]) {
-				checkRecycleList.add(edge.last);
+
+			if (prices[edge.start] != Integer.MIN_VALUE &&
+				prices[edge.last] < prices[edge.start] - edge.price + pay[edge.last]) {
+				check.add(edge.last);
+
 			}
 		}
 
-		if (distance[lastCity] == Integer.MIN_VALUE) {
+		if (prices[last] == Integer.MIN_VALUE) {
 			System.out.println("gg");
 			return;
 		}
 
-		if (!checkRecycleList.isEmpty()) {
-			for (int i = 0 ; i < checkRecycleList.size() ; i++) {
-				bfs(list, checkRecycleList.get(i));
-				if (visited[lastCity]) {
+		if (!check.isEmpty()) {
+			for (int i = 0 ; i < check.size() ; i++) {
+				int elem = check.get(i);
+				bfs(elem, list, visited);
+
+				if (visited[last]) {
 					System.out.println("Gee");
 					return;
 				}
 			}
 		}
-		System.out.println(distance[lastCity]);
+		System.out.println(prices[last]);
+
 	}
 }
-
