5c484df9539d6ef6fc645acdecf4c4e4327a8b9f
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2025-03-05 15:00:27 +0900

    [Gold IV] Title: 타임머신, Time: 252 ms, Memory: 19148 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.java
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -1,19 +1,14 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-class Edge{
-	int start;
-	int last;
-	int time;
+class Edge {
+	int start, end, time;
 
-	public Edge(int start, int last, int time) {
+	public Edge(int start, int end, int time) {
 		this.start = start;
-		this.last = last;
+		this.end = end;
 		this.time = time;
 	}
 }
@@ -25,55 +20,58 @@ public class Main {
 
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-		long dist[] = new long[n + 1];
+		long[] distance = new long[n + 1];
 		Edge edges[] = new Edge[m + 1];
 
-		Arrays.fill(dist, Integer.MAX_VALUE);
-		dist[1] = 0L;
-
-		for (int i = 0  ; i < m ; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-
-			edges[i] = new Edge(a, b, c);
+		for(int i = 1 ; i <= n ; i++) {
+			if (i != 1) {
+				distance[i] = Integer.MAX_VALUE;
+			}
 		}
 
-		for (int i = 1 ; i < n ; i++) {
-			for (int j = 0 ; j < m ; j++) {
-				Edge edge = edges[j];
+		for (int i = 0; i < m ; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			int last = Integer.parseInt(st.nextToken());
 
-				if (dist[edge.start] != Integer.MAX_VALUE
-				&& dist[edge.last] > dist[edge.start] + edge.time) {
-					dist[edge.last] = dist[edge.start] + edge.time;
+			edges[i] = new Edge(start, end, last);
+		}
+
+		for (int i = 1 ; i < n; i++) { // n-1까지 반복
+			for (int j = 0 ; j < m ; j++) {
+				Edge now = edges[j];
+				if (distance[now.start] != Integer.MAX_VALUE
+				 && distance[now.end] > distance[now.start] + now.time) {
+					distance[now.end] = distance[now.start] + now.time;
 				}
 			}
 		}
 
-		boolean isTrue = false;
+		boolean flag = false;
 
 		for (int j = 0 ; j < m ; j++) {
-			Edge edge = edges[j];
+			Edge now = edges[j];
 
-			if (dist[edge.start] != Integer.MAX_VALUE
-				&& dist[edge.last] > dist[edge.start] + edge.time) {
-				isTrue = true;
+			if (distance[now.start] != Integer.MAX_VALUE
+			&& distance[now.end] > distance[now.start] + now.time) {
+				flag = true;
 			}
 		}
 
-		if (isTrue) {
+		if (flag) {
 			System.out.println("-1");
 		}
 		else {
-			for (int i = 2 ; i <= n ; i++) {
-				if (dist[i] == Integer.MAX_VALUE)
+			for (int i = 2; i <= n ; i++) {
+				if (distance[i] == Integer.MAX_VALUE) {
 					System.out.println("-1");
-				else
-					System.out.println(dist[i]);
+				}
+				else {
+					System.out.println(distance[i]);
+				}
 			}
 		}
-
 
 	}
 }
