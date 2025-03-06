6007d12562ebd680c9b10ea415e2091d43326e83
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2025-03-06 12:38:29 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 488 ms, Memory: 48832 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,105 +1,84 @@
-import com.sun.source.tree.Tree;
-
-import java.awt.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.LinkedList;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Edge {
-	int start;
-	int last;
-	int value;
+class hNode implements Comparable<hNode>{
+	int start,last, value;
 
-	public Edge(int start ,int last, int value) {
-		this.value = value;
+	public hNode(int start, int last, int value) {
 		this.start = start;
 		this.last = last;
+		this.value = value;
+	}
+
+	@Override
+	public int compareTo(hNode o) {
+		return this.value - o.value;
 	}
 }
 
 public class Main {
+	private static int parent[];
+	private static PriorityQueue<hNode> queue;
 
-	static PriorityQueue<Edge> edges;
-	static int array[];
-
-	static int parent[];
-
-	public static void union(int a, int b) {
-		a = find(a);
-		b = find(b);
-
-		if (a < b)
-			parent[b] = a;
-		else
-			parent[a] = b;
-	}
-	public static int find(int a) {
+	private static int find(int a) {
 		if (a == parent[a])
 			return a;
 		return parent[a] = find(parent[a]);
+	}
+
+	private static void union(int a, int b) {
+		a = find(a);
+		b = find(b);
+
+		if (a < b) {
+			parent[b] = a;
+		}
+		else {
+			parent[a] = b;
+		}
 	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int v = Integer.parseInt(st.nextToken());
-		int e = Integer.parseInt(st.nextToken());
-		long sum = 0;
-		edges = new PriorityQueue<>(Comparator.comparingInt(hEdge -> hEdge.value));
-		array = new int[v + 1];
-		parent = new int[v + 1];
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
 
-		for (int i = 1 ; i <= e ; i++) {
-			st = new StringTokenizer(br.readLine());
-			int start = Integer.parseInt(st.nextToken());
-			int last = Integer.parseInt(st.nextToken());
-			int value = Integer.parseInt(st.nextToken());
-			edges.add(new Edge(start, last, value));
-		}
+		parent = new int[n + 1];
+		queue = new PriorityQueue<>(Comparator.comparingInt(hNode -> hNode.value));
 
-		for (int i = 1 ; i <= v ; i++) {
+		for (int i = 1; i <= n ; i++ ) {
 			parent[i] = i;
 		}
 
-		// 로직 시작
-		// for (int i = 1; i <= v ; i ++) {
-		// 	Edge now = edges.poll();
-		//
-		// 	if (find(now.start) != find(now.start)) {
-		// 		union(now.start, now.last);
-		// 		sum += now.value;
-		// 	}
-		// }
+		for (int i = 1 ; i <= m ; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			int start = Integer.parseInt(st.nextToken());
+			int last = Integer.parseInt(st.nextToken());
+			int value = Integer.parseInt(st.nextToken());
+			queue.add(new hNode(start, last, value));
+		}
+
+		long sum = 0;
 		int i = 0;
-		while (i < v - 1 && !edges.isEmpty()) {
-			Edge now = edges.poll();
-			if (find(now.start) != find(now.last)) {
-				union(now.start, now.last);
-				sum += now.value;
+		while (i < n - 1 && !queue.isEmpty()) {
+			hNode elem = queue.poll();
+
+			if (find(elem.start) != find(elem.last)) {
+				sum += elem.value;
+				union(elem.start, elem.last);
 				i++;
 			}
+
 		}
 
 		System.out.println(sum);
-
-
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
