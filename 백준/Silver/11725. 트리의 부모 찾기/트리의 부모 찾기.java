bdbf040e77c2f5f11710e5284d36834af8427b00
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2025-03-10 14:16:12 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 568 ms, Memory: 65632 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,61 +1,79 @@
+
+
 import com.sun.source.tree.Tree;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.LinkedList;
-import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	private static ArrayList<Integer> list[];
-	private static int answer[];
-
+	private static int result[];
 	private static boolean visited[];
+	private static ArrayList<Integer> list[];
+	private static void dfs(int index) {
+
+		visited[index] = true;
+
+		for (int elem : list[index]) {
+			if (!visited[elem]) {
+				result[elem] = index;
+				dfs(elem);
+			}
+		}
+	}
+
+	private static void bfs(int index) {
+
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(index);
+		visited[index] = true;
+
+		while (!queue.isEmpty()) {
+			int elem = queue.poll();
+
+			for (int i : list[elem]) {
+				if (!visited[i]) {
+					visited[i] = true;
+					result[i] = elem;
+					queue.add(i);
+				}
+			}
+
+		}
+	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int n = Integer.parseInt(br.readLine());
-		list = new ArrayList[n + 1];
-		answer = new int[n + 1];
+		result = new int[n + 1];
 		visited = new boolean[n + 1];
+		list = new ArrayList[n + 1];
 
 		for (int i = 1 ; i <= n ; i++) {
 			list[i] = new ArrayList<>();
 		}
+		result[1] = 1;
 
-		for (int i = 1; i < n ; i++) {
+		for (int i = 1 ; i < n ; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 
-			int start = Integer.parseInt(st.nextToken());
-			int last = Integer.parseInt(st.nextToken());
-
-			list[start].add(last);
-			list[last].add(start);
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			list[a].add(b);
+			list[b].add(a);
 		}
 
-		dfs(1);
-
-		for (int i = 2 ; i <= n ; i++) {
-			bw.write(answer[i] + "\n");
+		// dfs(1);
+		bfs(1);
+		for (int i = 2 ; i <= n ; i++){
+			bw.write(result[i] + "\n");
 		}
-
 		bw.flush();
 		bw.close();
-	}
-
-	public static void dfs(int index) {
-		visited[index] = true;
-
-		for (int elem : list[index]) {
-			if (!visited[elem]) {
-				answer[elem] = index;
-				dfs(elem);
-			}
-		}
 	}
 }
