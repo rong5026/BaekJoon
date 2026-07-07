079a38a5cc310509f65aee6c90ab7589437cfe6e
YeongHwan Hong <77156858+rong5026@users.noreply.github.com>
2026-07-07 21:22:49 +0900

    [level 1] Title: 폰켓몬, Time: 2.71 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,19 +1,20 @@
-import java.util.HashSet;
+import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-      
-        HashSet<Integer> list = new HashSet<>();
-      
-        int max = nums.length / 2;
-        for(int elem : nums) {
-            list.add(elem);
-        }
-        if (max < list.size()) {
-            return max;
-        }
-        return list.size();
+   
+        HashMap<Integer, Integer> map = new HashMap<>();
         
+        for(int i = 0 ; i < nums.length ; i++) {
+            map.put(nums[i], map.getOrDefault(map.get(nums[i]), 0) + 1);
+        }
+        
+        int keyCnt = map.size();
+        
+        if (keyCnt < nums.length / 2)
+            return keyCnt;
+        else
+            return nums.length/2;
         
     }
 }
